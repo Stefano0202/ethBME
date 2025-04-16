eb157fe6ced31d32296082046e323b6838451ec5
Matty <mattybern01@gmail.com>
2025-04-16 15:48:24 +0200

    modificata pagina contatti e connection.php

--- connection.php
+++ connection.php
@@ -8,21 +8,6 @@ $dbname = "BestMountainEver";
 // Creazione della connessione
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Controllo della connessione
-if ($conn->connect_error) {
-    echo "Connessione fallita";
-}
-else
-    echo "Connessione riuscita!";
-
-$sql = "INSERT INTO Test (prova) VALUES ('ciaociao')";
-
-    try {
-        $stmt = $conn->prepare($sql);
-        $stmt->execute();
-
-        echo "Dati inseriti correttamente nella tabella!";
-    } catch (Exception $e) {
-        echo "Errore durante l'inserimento: " . $e->getMessage();
-    }
+ //$stmt = $conn->prepare($sql);
+        
 ?>
