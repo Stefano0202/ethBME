2502db0c02b5990dcadc02244ed7e51b0f734400
Invj21 <137944530+Invj21@users.noreply.github.com>
2025-04-23 15:54:06 +0200

    Update controlloCredenziali.php

--- controlloCredenziali.php
+++ controlloCredenziali.php
@@ -1,41 +1,38 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require 'connection.php';
-// Configurazione
+
 define('SECRET_KEY', '%Ssgo*&94W$3AG1@M0Z8');
 
-// Funzione per generare un token
 function generaToken($username) {
     $payload = [
         'username' => $username,
-        'scadenza' => time() + 3600 // 1 ora di validità
+        'scadenza' => time() + 3600
     ];
     return base64_encode(json_encode($payload)) . '.' . hash_hmac('sha256', json_encode($payload), SECRET_KEY);
 }
 
-// Login dell'utente
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     global $pdo;
 
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    // Prepara la query per verificare l'utente nel database
-    $stmt = $pdo->prepare("SELECT password FROM Utenti WHERE username = " + $username + ";");
-    //$stmt->bindParam(':username', $username);
+    $stmt = $pdo->prepare("SELECT password FROM Utenti WHERE username = :username");
+    $stmt->bindParam(':username', $username);
     $stmt->execute();
-
-    var_dump($stmt);
 
     $user = $stmt->fetch(PDO::FETCH_ASSOC);
 
-    echo "Prima dell'if";
+    echo "Prima dell'if<br>";
 
-    if ($user && password_verify($password, $user['password'])) { // Confronta la password usando password_verify
-        echo "Prima della generazione del token";
+    if ($user && password_verify($password, $user['password'])) {
+        echo "Prima della generazione del token<br>";
         $token = generaToken($username);
 
-        echo "Dopo l'if";
-        // Imposta il cookie di autenticazione
         setcookie('authToken', $token, [
             'expires' => time() + 3600,
             'path' => '/',
