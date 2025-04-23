1da60be59ad4ddcb662660ea287ec1660332af24
Stefano0202 <165303517+Stefano0202@users.noreply.github.com>
2025-04-23 15:41:52 +0200

    Update connection.php

--- connection.php
+++ connection.php
@@ -8,6 +8,17 @@ $dbname = "BestMountainEver";
 // Creazione della connessione
 $conn = new mysqli($servername, $username, $password, $dbname);
 
- //$stmt = $conn->prepare($sql);
+try {
+    // Creazione dell'istanza PDO
+    $dsn = "mysql:host=$servername;dbname=$dbname;charset=utf8mb4";
+    $pdo = new PDO($dsn, $username, $password);
+
+    // Impostazione degli attributi PDO
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+} catch (PDOException $e) {
+    // Gestione dell'errore di connessione
+    die("Errore di connessione: " . $e->getMessage());
+}
         
 ?>
