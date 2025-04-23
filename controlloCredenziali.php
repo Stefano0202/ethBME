8fb4f3bac2429d8469537db9cf72ae35505647e9
Stefano0202 <165303517+Stefano0202@users.noreply.github.com>
2025-04-23 15:26:15 +0200

    Update controlloCredenziali.php

--- controlloCredenziali.php
+++ controlloCredenziali.php
@@ -18,6 +18,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
+    echo "Username: " . htmlspecialchars($username) . "<br>";
+    echo "Password: " . htmlspecialchars($password) . "<br>";
+
     // Prepara la query per verificare l'utente nel database
     $stmt = $pdo->prepare("SELECT password FROM utenti WHERE username = :username");
     $stmt->bindParam(':username', $username);
