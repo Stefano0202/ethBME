a01ea6e56c031855a5492b1c67135178c149b7d0
Invj21 <137944530+Invj21@users.noreply.github.com>
2025-04-23 16:43:42 +0200

    Update upload.php

---
+++ upload.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Upload Immagini</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="upload-container">
+        <button id="uploadButton">UPLOAD</button>
+        <input type="file" id="fileInput" multiple style="display: none;">
+    </div>
+    <script src="script.js"></script>
+</body>
+</html>
